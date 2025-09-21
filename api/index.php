fb66e482d4be98bb06860de6d017813163e11505
bryan123038 <bryan.ginting.bg@gmail.com>
2025-09-21 19:18:33 +0700

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,15 @@
+{
+  "profile": {
+    "name": "Bryan Ginting",
+    "title": "Web Developer | Tech Enthusiast",
+    "image": "/images/profile.jpg",
+    "bio": "Saya seorang pengembang web..."
+  },
+  "skills": ["PHP", "JavaScript", "CSS", "MySQL", "Laravel", "React"],
+  "projects": [...],
+  "socials": {
+    "GitHub": "https://github.com/...",
+    "LinkedIn": "https://linkedin.com/...",
+    "Instagram": "https://instagram.com/..."
+  }
+}
